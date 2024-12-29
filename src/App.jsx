b4a91ee048a36c48716af66951b0467f7a4a1b34
nrjrajpal <nrjrpl007@gmail.com>
2024-12-29 13:15:51 +0530

    add hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
+import Hero from './components/Hero'
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-5xl font-bold text-violet-300">
-        Hello world!!
-      </h1>
-    </div>
+    <main className='relative min-h-screen w-screen overflow-x-hidden'>
+      <Hero />
+      <section className='z-0 min-h-screen bg-blue-500'/>
+    </main>
   )
-}
+} 
 
 export default App
